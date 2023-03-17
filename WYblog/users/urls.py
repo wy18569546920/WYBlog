1e4332f773e5df507f9c08fba6d8ba553c8b21dd
wy <1195139055@qq.com>
2023-03-17 21:02:11 +0800

    图片验证码

--- WYblog/users/urls.py
+++ WYblog/users/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
-from users.views import RegisterView
+from users.views import RegisterView, ImageCodeView
 
 urlpatterns = [
+    # 注册
     path('register/', RegisterView.as_view(), name='register'),
+
+    # 图片验证码
+    path('imagecode/', ImageCodeView.as_view(), name='imagecode'),
 ]
--- WYblog/users/views.py
+++ WYblog/users/views.py
@@ -1,11 +1,35 @@
 from django.shortcuts import render
-
-# Create your views here.
+from django.http.response import HttpResponseBadRequest
+from django.http import HttpResponse
+from libs.captcha.captcha import captcha
+from django_redis import get_redis_connection
 from django.views import View
 
 
+# 注册视图
 class RegisterView(View):
 
     def get(self, request):
-
         return render(request, 'register.html')
+
+
+class ImageCodeView(View):
+
+    def get(self, request):
+        """
+        :param request:
+        :return:
+        """
+        # 1. 接受前端传的uuid
+        uuid = request.GET.get('uuid')
+        # 2. 判断uuid是否获取到
+        if uuid is None:
+            return HttpResponseBadRequest('not uuid')
+        # 3. 通过调用captcha来生成图片验证码（图片二进制和图片内容）
+        text, image = captcha.generate_captcha()
+        # 4. 将图片内容保存到redis中
+        #     uuid作为一个key,图片内容作为一个value. 同时添加一个过期时间
+        redis_conn = get_redis_connection('default')  # 使用redis配置文件中的默认配置
+        redis_conn.setex('img: %s' % uuid, 300, text)
+        # 5. 返回图片二进制
+        return HttpResponse(image, content_type='image/jpeg')
